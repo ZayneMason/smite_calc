3a4ccd12c9e03ea138cbef1db958a072df16d69a
ZayneMason <zayne.c.mason@gmail.com>
2021-09-22 00:50:07 -0700

    fixed some bullshit

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
-Dir["/home/zayne/smite_calc/lib/*/*"].each {|file| require file }
+Dir["/home/*/smite_calc/lib/*/*"].each {|file| require file }
 
 ra = Ra.new(@ra_stats, "Ovaculos")
 nezha = Nezha.new(@nezha_stats, "Gabereal")
