dabfd89798f3027420f94b0afc50223215a47fd8
Mason Abbruzzese <masonderolo@gmail.com>
2021-11-22 13:54:54 -0700

    I forgot Co-authored-by: ZayneMason <ZayneMason@users.noreply.github.com>

--- lib/item_stuff/item.rb
+++ lib/item_stuff/item.rb
@@ -1,8 +1,24 @@
-require_relative "item_class.rb"
 
 class Item
+  attr_accessor :item_stats
   def initialize
-    
+    @item_stats = 0
+  end
+
+  def holder(holder)
+    @holder = holder
+  end
+  
+end
+
+class Test_Item < Item
+  def initialize
+    @item_stats = {health: 200, mana: 100}
+  end
+  
+  def holder(holder)
+    super(holder)
+    puts "#{holder.stats[:name]}"
   end
 
 end
